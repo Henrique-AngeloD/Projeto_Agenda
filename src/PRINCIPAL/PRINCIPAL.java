a823eb29f8aef8440488205e6e5228b2a85eb607
Henrique-AngeloD <henriqueangeli.alves@gmail.com>
2025-02-14 15:49:17 -0300

    Concerto

--- src/PRINCIPAL/PRINCIPAL.java
+++ src/PRINCIPAL/PRINCIPAL.java
@@ -4,17 +4,23 @@
  */
 package PRINCIPAL;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author henri
  */
 public class PRINCIPAL extends javax.swing.JFrame {
-
-    /**
-     * Creates new form PRINCIPAL
-     */
+    int AnoFIXO = 0, MesFIXO = 0, DiaFIXO = 0;
+    int AnoAltera = 0, MesAltera = 0, DiaALTERA = 0;
+    int valorselecao = 0;
+    
+    /** Creates new form PRINCIPAL*/
     public PRINCIPAL() {
         initComponents();
+        INICIACALENDARIO();
     }
 
     /**
@@ -470,7 +476,7 @@ public class PRINCIPAL extends javax.swing.JFrame {
                 .addComponent(jLayeredPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addContainerGap(415, Short.MAX_VALUE))
+                .addContainerGap(383, Short.MAX_VALUE))
         );
         jLayeredPane1Layout.setVerticalGroup(
             jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -479,7 +485,7 @@ public class PRINCIPAL extends javax.swing.JFrame {
                 .addGroup(jLayeredPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButton1)
                     .addComponent(jLayeredPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(220, Short.MAX_VALUE))
+                .addContainerGap(233, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -490,13 +496,10 @@ public class PRINCIPAL extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLayeredPane1)
-                .addContainerGap())
+            .addComponent(jLayeredPane1)
         );
 
-        setBounds(0, 0, 839, 507);
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void SelecaoMesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SelecaoMesActionPerformed
@@ -599,4 +602,21 @@ public class PRINCIPAL extends javax.swing.JFrame {
     private javax.swing.JLabel t8;
     private javax.swing.JLabel t9;
     // End of variables declaration//GEN-END:variables
+
+    //COMEÇA CONSTRUÇÃO DO CALENDARIO
+    public void INICIACALENDARIO(){
+        SimpleDateFormat Ano = new SimpleDateFormat("yyyy");
+        SimpleDateFormat Mes = new SimpleDateFormat("MM");
+        SimpleDateFormat Dia = new SimpleDateFormat("dd");
+        
+        AnoFIXO = Integer.parseInt(Ano.format(new Date()));
+        MesFIXO = Integer.parseInt(Mes.format(new Date()));
+        DiaFIXO = Integer.parseInt(Dia.format(new Date()));
+        
+        JOptionPane.showMessageDialog(null, DiaFIXO+"/"+MesFIXO+"/"+AnoFIXO);
+        
+    }
+
+
+
 }
